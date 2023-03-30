946a4f90fd01e094c2e08f9b92d17c8034c77834
kohei-sa <kohei-sa@primagest.co.jp>
2023-03-30 11:36:26 +0900

    動作確認前のコミット

--- routers/route_todo.py
+++ routers/route_todo.py
@@ -1,38 +1,57 @@
 # タスク関係のルーター
-from fastapi import APIRouter, Depends
-from fastapi import Response, Request, HTTPException
+from fastapi import APIRouter
+from fastapi import Response, Request, HTTPException, Depends
+from fastapi_csrf_protect import CsrfProtect
 from sqlalchemy.orm import Session
 import schemas
 import crud
 from starlette.status import HTTP_201_CREATED
 from .get_db import get_db
+from auth_utils import AuthJwtCsrf
 
 router = APIRouter()
-
-
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+auth = AuthJwtCsrf()
 
 
 @router.post("/api/todo/", response_model=schemas.Todo)
 def create_todo(request: Request, response: Response,
-                todo: schemas.TodoCreate, db: Session = Depends(get_db)):
+                todo: schemas.TodoCreate,
+                db: Session = Depends(get_db),
+                csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(
+        request, csrf_protect, request.headers)
     response.status_code = HTTP_201_CREATED
-    return crud.create_todo(db=db, todo=todo)
+    response.set_cookie(
+        key="access_token",
+        value=f"Bearer {new_token}",
+        httponly=True, samesite="none",
+        secure=True)
+    res = crud.create_todo(db=db, todo=todo)
+    if res:
+        return res
+    raise HTTPException(status_code=404, detail="Create task faild")
 
 
 @router.get("/api/todo/", response_model=list[schemas.Todo])
-def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_todos(request: Request,
+               skip: int = 0, limit: int = 100,
+               db: Session = Depends(get_db)):
+    # auth.verify_jwt(request)
     todos = crud.get_todos(db, skip=skip, limit=limit)
     return todos
 
 
 @router.get("/api/todo/{todo_id}", response_model=schemas.Todo)
-def read_todo(todo_id: int, db: Session = Depends(get_db)):
+def read_todo(request: Request, response: Response,
+              todo_id: int,
+              db: Session = Depends(get_db),
+              csrf_protect: CsrfProtect = Depends()):
+    new_token, _ = auth.verify_update_jwt(request)
+    response.set_cookie(
+        key="access_token",
+        value=f"Bearer {new_token}",
+        httponly=True, samesite="none",
+        secure=True)
     db_todo = crud.get_todo(db, todo_id=todo_id)
     if db_todo is None:
         raise HTTPException(status_code=404, detail="Todo not found")
@@ -40,8 +59,17 @@ def read_todo(todo_id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/api/todo/{todo_id}", response_model=schemas.Todo)
-def update_todo(todo_id: int, data: schemas.TodoCreate,
-                db: Session = Depends(get_db)):
+def update_todo(request: Request, response: Response,
+                todo_id: int, data: schemas.TodoCreate,
+                db: Session = Depends(get_db),
+                csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(
+        request, csrf_protect, request.headers)
+    response.set_cookie(
+        key="access_token",
+        value=f"Bearer {new_token}",
+        httponly=True, samesite="none",
+        secure=True)
     db_todo = crud.update_todo(db, todo_id=todo_id, data=data)
     if db_todo:
         return db_todo
@@ -49,7 +77,16 @@ def update_todo(todo_id: int, data: schemas.TodoCreate,
 
 
 @router.delete("/api/todo/{todo_id}", response_model=schemas.SuccessMessage)
-def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+def delete_todo(request: Request, response: Response,
+                todo_id: int, db: Session = Depends(get_db),
+                csrf_protect: CsrfProtect = Depends()):
+    new_token = auth.verify_csrf_update_jwt(
+        request, csrf_protect, request.headers)
+    response.set_cookie(
+        key="access_token",
+        value=f"Bearer {new_token}",
+        httponly=True, samesite="none",
+        secure=True)
     is_delete = crud.delete_todo(db, todo_id=todo_id)
     if is_delete:
         return {"message": "Successfully deleted"}

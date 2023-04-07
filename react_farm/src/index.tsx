2cd9e15114bc4878294ad90d3cbf4d0bcaebcb92
kohei-sa <kohei-sa@primagest.co.jp>
2023-04-07 14:24:00 +0900

    hooksの追加と設定

--- react_farm/src/index.tsx
+++ react_farm/src/index.tsx
@@ -5,15 +5,29 @@ import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+})
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
